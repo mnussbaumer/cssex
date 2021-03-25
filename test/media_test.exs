1ecd8d2bd01b68b9577bbc95e8990ce457e57dc9
Mickey Mouse <micaelnussbaumer@gmail.com>
2021-03-25 19:19:32 +0800

    working base for nested media

--- test/media_test.exs
+++ test/media_test.exs
@@ -2,48 +2,58 @@ defmodule CSSEx.Media.Test do
   use ExUnit.Case, async: true
   alias CSSEx.Parser
 
-  @base_nested """
-  .test {
-    @media screen and (max-width: 600px) {
-      div.example {
-        display: none;
-      }
-
-      font-family: Arial;
-    }
-        	
-    color: red;
-  }
-
-  @media screen and (max-width: 600px) {
-    .test { background-color: black; }
-  }
-  """
-
-  @vars_in_media_declaration """
-  @!max-width 565px;
-  .test {
-    @media print and (max-width: @$$max-width and min-width: 300px), (min-width: @$$max-width) {
-      color: blue;
-    }
-  }
-  """
-
   test "nested media generates the correct css rules and doesn't generate a media query nested into a selector" do
-    assert {:ok, _, parsed} = Parser.parse(@base_nested)
+    assert {:ok, _, parsed} =
+             Parser.parse("""
+             .test {
+               @media screen and (max-width: 600px) {
+                 div.example {
+                   display: none;
+                 }
 
-    [
-      "@media screen and (max-width:600px){.test{font-family:Arial;background-color:black;}.test div.example{display:none;}}\n",
-      ".test{color:red;}"
-    ]
-    |> Enum.each(fn verification ->
-      assert parsed =~ verification
-    end)
+                 font-family: Arial;
+               }
+                   	
+               color: red;
+             }
+
+             @media screen and (max-width: 600px) {
+               .test { background-color: black; }
+             }
+             """)
+
+    assert parsed =~
+             "@media screen and (max-width:600px){.test{font-family:Arial;background-color:black;}.test div.example{display:none;}}\n"
+
+    assert parsed =~ ".test{color:red;}"
   end
 
   test "replacement of variables inside media works correctly" do
     assert {:ok, _,
             "@media print and (max-width:565px and min-width:300px), (min-width:565px){.test{color:blue;}}\n"} =
-             Parser.parse(@vars_in_media_declaration)
+             Parser.parse("""
+             @!max-width 565px;
+             .test {
+               @media print and (max-width: @$$max-width and min-width: 300px), (min-width: @$$max-width) {
+                 color: blue;
+               }
+             }
+             """)
+  end
+
+  test "nested media components compose" do
+    assert {:ok, _,
+            "@media print and (max-width:565px){.test{color:red;}}@media print and (max-width:565px) and (min-width:300px){.test{color:orange;}.test.inner{color:blue;}}\n"} =
+             Parser.parse("""
+             @!color orange;
+             @!min_width 300px;
+             @media print and (max-width: 565px) {
+               .test { color: red;
+             	@media and (min-width: <$min_width$>) {
+                  &.inner { color: blue; }
+             	   color: <$color$>;
+               }
+             }}
+             """)
   end
 end
