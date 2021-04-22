23bf34ec623ffac3799f9c5ecc5c160897d45f07
Mickey Mouse <micaelnussbaumer@gmail.com>
2021-04-22 15:36:07 +0800

    bump v

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cssex.MixProject do
   def project do
     [
       app: :cssex,
-      version: "0.6.6",
+      version: "0.6.7",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
