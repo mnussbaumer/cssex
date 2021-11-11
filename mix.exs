a21a16e5e6970dfc25c46b3c542ea77de394adb4
Mickey Mouse <micaelnussbaumer@gmail.com>
2021-11-11 08:02:52 +0800

    Temp fix for attributes with a colon that might split into more than one - this only takes care for  rules but TODO proper fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cssex.MixProject do
   def project do
     [
       app: :cssex,
-      version: "0.6.8",
+      version: "0.6.9",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
